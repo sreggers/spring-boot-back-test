9f2e32f5deafe450151216d7cf91b07f3706e51f
sreggers <sebastiaan@reggers.org>
2020-07-07 17:39:54 +0200

    Add and refactor Lombok

--- src/main/java/be/springboot/demo/User.java
+++ src/main/java/be/springboot/demo/User.java
@@ -1,10 +1,12 @@
 package be.springboot.demo;
 
 import com.google.cloud.firestore.annotation.DocumentId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.cloud.gcp.data.firestore.Document;
 
-import java.util.Objects;
-
+@Data @AllArgsConstructor @NoArgsConstructor
 @Document(collectionName = "users")
 public class User {
 
@@ -12,50 +14,5 @@ public class User {
     String name;
 
     String email;
-
-    public User() {}
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) &&
-                Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, email);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 
 }
