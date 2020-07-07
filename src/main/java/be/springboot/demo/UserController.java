c517ebd63cd72eabd9304ab705cd91bcb6927752
sreggers <sebastiaan@reggers.org>
2020-07-07 17:19:46 +0200

    Update CORS

--- src/main/java/be/springboot/demo/UserController.java
+++ src/main/java/be/springboot/demo/UserController.java
@@ -5,7 +5,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "http://sreggers.github.io")
+@CrossOrigin(origins = "https://sreggers.github.io")
 public class UserController {
 
     private final UserRepository userRepository;
